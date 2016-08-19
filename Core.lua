f503f5f554fc8e2792a6536c1b5d4211f22e8757
Brianna Buckner <pdx.bucknerb@gmail.com>
2016-08-19 16:38:00 -0700

    initial commit + Ac3 and LibWindow

---
+++ Core.lua
@@ -0,0 +1,12 @@
+--[[  LootSecretary by Annasia
+Core.lua        Contains core elements of the addon
+
+-----------
+TODO/Notes
+    The whole addon
+    https://etherpad.openstack.org/p/PL_council
+
+]]
+
+LootSecretary = LibStub("AceAddon-3.0"):NewAddon("LootSecretary", "AceConsole-3.0", "AceEvent-3.0", "AceComm-3.0");
+local libwin = LibStub("LibWindow-1.1");
