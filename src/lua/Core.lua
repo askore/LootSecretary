7f75523aea3a7855e2af09c11c84040eb3943741
Brianna Buckner <pdx.bucknerb@gmail.com>
2016-08-22 23:24:56 -0700

    make embeds go

--- src/lua/Core.lua
+++ src/lua/Core.lua
@@ -8,8 +8,30 @@ TODO/Notes
 
 ]]
 
--- LootSecretary = LibStub("AceAddon-3.0"):NewAddon("LootSecretary", "AceConsole-3.0", "AceEvent-3.0", "AceComm-3.0");
--- local libwin = LibStub("LibWindow-1.1");
--- local mFrame = CreateFrame("Frame")
+local LootSecretary = LibStub("AceAddon-3.0"):NewAddon("LootSecretary", "AceConsole-3.0", "AceEvent-3.0")
+local libwin = LibStub("LibWindow-1.1")
+local mFrame = CreateFrame("Frame")
 
-print("Hello world")
+LootSecretary:Print("Hello, world!")
+function LootSecretary:OnInitialize()
+	LootSecretary:Print("Welcome to OnInit")
+	-- create the table to hold loot listings
+end
+
+function LootSecretary:OnEnable()
+	LootSecretary:Print("Welcome to OnEnable")
+end
+
+function LootSecretary:OnDisable()
+	LootSecretary:Print("Welcome to OnDisable")
+end
+
+LootSecretary:RegisterChatCommand("lsc", "LSConfig")
+function LootSecretary:LSConfig(input)
+	LootSecretary:Print("This is the config")
+end
+
+function LootSecretary:CHAT_MSG_LOOT(message, sender, ...)
+	LootSecretary:Print(sender)
+end
+LootSecretary:RegisterEvent("CHAT_MSG_LOOT")
